4ea19673180e17abdbe0ccb67ce422bb4e84c39e
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-31 11:42:39 +0300

    Update StringRole.java

--- src/App/StringRole.java
+++ src/App/StringRole.java
@@ -2,22 +2,20 @@ package App;
 
 public class StringRole {
 
-    public String[] SortRole(String[] text, String[] roles) {
+    public String[] printTextPerRole(String[] text, String[] roles) {
 
-        String[] newText = new String[roles.length * 2];
+        String[] newText = new String[roles.length];
 
         for (int i = 0; i < roles.length; i++) {
-            vewText[i] = roles[i];
-        }
-        for (int i = roles.length; i < roles.length * 2; i++) {
             StringBuilder buffer = new StringBuilder();
-            buffer.append("\n")
-                    .append(roles[i - roles.length])
+            buffer.append(roles[i])
                     .append(": \n");
             for (int j = 0; j < text.length; j++) {
-                if (text[j].startsWith(roles[i - roles.length])) {
+                if (text[j].startsWith(roles[i])) {
                     buffer
-                            .append(text[j].replace(roles[i - roles.length], Integer.toString(j + 1)).replace(':', ')'))
+                            .append(text[j]
+                                    .replace(roles[i], (j + 1)+")")
+                                    .replace("):", ")"))
                             .append("\n");
                     newText[i] = buffer.toString();
                 }
