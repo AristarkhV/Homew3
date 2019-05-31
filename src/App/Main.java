f60787cd89cba5e864bc4eddebb66c2653802b22
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-31 11:44:11 +0300

    Update Main.java

--- src/App/Main.java
+++ src/App/Main.java
@@ -6,23 +6,23 @@ public class Main {
 
         int[] firstArray = {0, 2, 2, 8},
                 secondArray = {1, 3};
-        MargeSort bufferArray = new MargeSort();
-        int[] resultArray = bufferArray.Sort(firstArray, secondArray);
+        MargeArrays bufferArray = new MargeArrays();
+        int[] resultArray = bufferArray.mergeArrays(firstArray, secondArray);
         for (int i = 0; i < 6; i++) System.out.print(resultArray[i] + " ");
         System.out.println("\n\n");
-        String[] textLines = {"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие:к нам едет ревизор.",
+        String [] roles= {
+                "Городничий","Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String [] textLines={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
                 "Аммос Федорович: Как ревизор?",
                 "Артемий Филиппович: Как ревизор?",
                 "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
                 "Аммос Федорович: Вот те на!",
                 "Артемий Филиппович: Вот не было заботы, так подай!",
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
-        String[] roles = {"Городничий",
-                "Аммос Федорович",
-                "Артемий Филиппович",
-                "Лука Лукич"};
         StringRole text = new StringRole();
-        String[] buffer = text.SortRole(textLines, roles);
+        String[] buffer = text.printTextPerRole(textLines, roles);
         for (int i = 0; i < buffer.length; i++) {
             System.out.println(buffer[i]);
         }
